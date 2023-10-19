f70bd8eb59220c81be07622067563c6bc3c135df
martinfigue <martin.figueroahdez@gmail.com>
2023-10-19 11:53:08 -0300

    error arreglado en precios

--- scr/Precios.java
+++ scr/Precios.java
@@ -2,9 +2,9 @@ public enum Precios{
 
     PRECIOCOCA(700), PRECIOSPRITE(700), PRECIOFANTA(700), PRECIOSNICKERS(900), PRECIOSUPER8(300);
 
-    private final precio;
+    private final int precio;
 
-    public Precios(int precio){
+    Precios(int precio){
         this.precio = precio;
     }
 
