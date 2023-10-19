bb7dedd4beb65452a88c6b97543a1999a1c202cb
idiazbarrientos <146099724+idiazbarrientos@users.noreply.github.com>
2023-10-19 13:08:32 -0300

    Creacion del primer test de expendedor

---
+++ Test/ExpendedorTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExpendedorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void comprarProducto() {
+    }
+
+    @Test
+    void getVuelto() {
+    }
+}
