ce4645ec42ce70a63e85b2b8ae9178807b0d0774
martinfigue <martin.figueroahdez@gmail.com>
2023-10-18 22:08:12 -0300

    enumeración de precios y sus métodos

---
+++ Precios.java
@@ -0,0 +1,14 @@
+public enum Precios{
+
+    PRECIOCOCA(700), PRECIOSPRITE(700), PRECIOFANTA(700), PRECIOSNICKERS(900), PRECIOSUPER8(300);
+
+    private final precio;
+
+    public Precios(int precio){
+        this.precio = precio;
+    }
+
+    public int getPrecio(){
+        return precio;
+    }
+}
