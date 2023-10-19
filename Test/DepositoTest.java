ed5b7c18bebce3293ba4a75fe6610e6451c0fb88
idiazbarrientos <146099724+idiazbarrientos@users.noreply.github.com>
2023-10-19 20:17:02 -0300

    creacion de los test

---
+++ Test/DepositoTest.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DepositoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void addDulce() {
+    }
+
+    @Test
+    void addBebida() {
+    }
+
+    @Test
+    void addMoneda() {
+    }
+
+    @Test
+    void getBebida() {
+    }
+
+    @Test
+    void getDulce() {
+    }
+
+    @Test
+    void getMoneda() {
+    }
+}
