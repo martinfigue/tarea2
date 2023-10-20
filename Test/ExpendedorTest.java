4b890324f8569236b135e2fa3a2ca60f0cfa535f
idiazbarrientos <146099724+idiazbarrientos@users.noreply.github.com>
2023-10-20 00:05:15 -0300

    test de compra y una excepcion
    test de que la compra funciona correctamente y que la excepcion no hay producto funcione correctamente

--- Test/ExpendedorTest.java
+++ Test/ExpendedorTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,7 +9,7 @@ class ExpendedorTest {
 
     @BeforeEach
     void setUp() {
-        Expendedor exp = new Expendedor(2, 300);
+        Expendedor exp = new Expendedor(100, 300);
     }
 
     @AfterEach
@@ -16,16 +17,32 @@ class ExpendedorTest {
     }
 
     @Test
-    void pruebaCompraruUnProducto() throws NoHayProductoException {
-        Expendedor exp = new Expendedor(2, 300);
+    @DisplayName("Test comprar un prodcuto")
+    void testComprarUnProducto() throws Exception {
+        Expendedor exp = new Expendedor(3, 700);
         Moneda m;
         Comprador c;
-        m = new Moneda500();
-        c = new Comprador(m,Expendedor.FANTA,exp);
+        Comprador a;
+        Comprador l;
+        m = new Moneda1000();
+        c = new Comprador(m,Expendedor.COCA,exp);
         System.out.println(c.queConsumiste()+", "+c.cuantoVuelto());
+        a = new Comprador(m,Expendedor.FANTA,exp);
+        System.out.println(a.queConsumiste()+", "+a.cuantoVuelto());
+        l = new Comprador(m,Expendedor.SPRITE,exp);
+        System.out.println(l.queConsumiste()+", "+l.cuantoVuelto());
     }
-
     @Test
-    void pruebagetVuelto() {
+    @DisplayName("NoHayProductoException")
+    void testComprar2Productos() throws Exception {
+        Expendedor exp = new Expendedor(1, 900);
+        Moneda m;
+        Comprador c;
+        Comprador i;
+        m = new Moneda1000();
+        c = new Comprador(m, Expendedor.SNICKERS, exp);
+        System.out.println(c.queConsumiste() + ", " + c.cuantoVuelto());
+        i = new Comprador(m, Expendedor.SNICKERS, exp);
+        System.out.println(i.queConsumiste() + ", " + c.cuantoVuelto());
     }
 }
