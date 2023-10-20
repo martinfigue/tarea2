31d412a5be9cde23d3900642b8b9449b6e1724da
martinfigue <martin.figueroahdez@gmail.com>
2023-10-20 17:36:26 -0300

    Deposito: modificación de la clase Deposito para hacer uno de tipo genérico

--- scr/Deposito.java
+++ scr/Deposito.java
@@ -1,48 +1,20 @@
 import java.util.ArrayList;
 
-public class Deposito{
-    private ArrayList<Bebida> a1;
-    private ArrayList<Moneda> a2;
-    private ArrayList<Dulce> a3;
+public class Deposito<T>{
+    private ArrayList<T> elementos;
     public Deposito(){
-        a1 = new ArrayList();
-        a2 = new ArrayList();
-        a3 = new ArrayList();
+        elementos = new ArrayList();
     }
-    public void addDulce(Dulce d){
-        a3.add(d);
+    public void addElemento(T elemento){
+        elementos.add(elemento);
     }
-    public void addBebida(Bebida b){
-        a1.add(b);
-    }
-    public void addMoneda(Moneda m){
-        a2.add(m);
-    }
-    public Bebida getBebida(){
-        if(a1.size() == 0){
+
+    public T getElemento() {
+        if (elementos.size() == 0) {
             return null;
-        }
-        else{
-            Bebida b = a1.remove(0);
-            return b;
-        }
-    }
-    public Dulce getDulce(){
-        if(a3.size() == 0){
-            return null;
-        }
-        else{
-            Dulce d = a3.remove(0);
-            return d;
-        }
-    }
-    public Moneda getMoneda(){
-        if(a2.size() == 0){
-            return null;
-        }
-        else{
-            Moneda m = a2.remove(0);
-            return m;
+        } else {
+            T elemento = elementos.remove(0);
+            return elemento;
         }
     }
 }
