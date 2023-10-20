5158bd05a32e58adf9986f7bdaa5640e33b1c4ed
idiazbarrientos <146099724+idiazbarrientos@users.noreply.github.com>
2023-10-20 00:04:21 -0300

    No hay producto exception en main

--- scr/Main.java
+++ scr/Main.java
@@ -1,10 +1,18 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        Expendedor exp = new Expendedor(5,300);
-        Moneda m;
-        Comprador c;
-        m = new Moneda1000();
-        c = new Comprador(m,Expendedor.COCA,exp);
-        System.out.println(c.queConsumiste()+", "+c.cuantoVuelto());
+        try {
+            Expendedor exp = new Expendedor(1, 900);
+            Moneda m;
+            Comprador c;
+            Comprador i;
+            m = new Moneda1000();
+            c = new Comprador(m, Expendedor.SNICKERS, exp);
+            System.out.println(c.queConsumiste() + ", " + c.cuantoVuelto());
+            i = new Comprador(m, Expendedor.SNICKERS, exp);
+            System.out.println(i.queConsumiste() + ", " + c.cuantoVuelto());
+        }
+        catch (Exception NoHayProductoException) {
+                System.out.println("No hay mas de este producto");
+        }
     }
 }
