6e4928971853a3d0605b77c58df561a045f77a8d
martinfigue <martin.figueroahdez@gmail.com>
2023-10-20 22:16:11 -0300

    Documentación Precios

--- scr/Precios.java
+++ scr/Precios.java
@@ -1,6 +1,17 @@
+/**
+ * El enum con los precios
+ * @author ignaciodiaz
+ * @author martinfigueroa
+ */
+
 public enum Precios{
 
     PRECIOCOCA(700), PRECIOSPRITE(700), PRECIOFANTA(700), PRECIOSNICKERS(900), PRECIOSUPER8(300);
+
+    /** Los precios de cada producto
+     *
+     * @param precio asocia un precio a cada constante
+     */
 
     private final int precio;
 
